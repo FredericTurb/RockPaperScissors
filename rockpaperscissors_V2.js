0f925dd4c82a1f06f1357253b9b3cd044c5b934b
FredericTurb <185390564+FredericTurb@users.noreply.github.com>
2024-10-25 05:55:19 +0200

    Rock_Paper_Scissors V2

--- rockpaperscissors_V2.js
+++ rockpaperscissors_V2.js
@@ -4,10 +4,15 @@ const buttonRock = document.querySelector("#rock");
 const buttonPaper = document.querySelector("#paper");
 const buttonScissors = document.querySelector("#scissors");
 
+const won = "You Won This Round !";
+const lost = "You Lost This Round !";
+const tie = "This Is a Tie ! No points !";
+
+const wonGame = "CONGRATULATIONS ! You Won The Game ! ";
+const lostGame = "You Lost Sucker ! Try Again ! ";
+
 let humanScore = 0;
 let computerScore = 0;
-let computerChoice = getComputerChoice();
-
 
 function getComputerChoice(){
     let myRandom = Math.floor(Math.random() * 3);
@@ -20,23 +25,64 @@ function getHumanChoice(){
 
 function playGame(myHuman){
     computerChoice = getComputerChoice();
+    
     if(myHuman === 0 && computerChoice === mainArray[2] || myHuman === 1 && computerChoice === mainArray[0] || myHuman === 2 && computerChoice === mainArray[1]){
-            humanScore = humanScore + 1;
-            checkScores();}
-    else if(myHuman === 0 && computerChoice === mainArray[1] || myHuman === 1 && computerChoice === mainArray[2] || myHuman === 2 && computerChoice === mainArray[0]){
-            computerScore = computerScore + 1;
-            checkScores();}
+            humanScore++;
+            displayInDivRound(won);}
+    else if(myHuman === 0 && computerChoice === mainArray[1] || myHuman === 1 && computerChoice === mainArray[2] || myHuman === 2 && computerChoice === mainArray[0]){    
+            computerScore++;
+            displayInDivRound(lost);}
+    else{displayInDivRound(tie);}
+
+    displayPlayed(myHuman)
+    checkScores();
+    displayScores()  
 }
 
 function checkScores(){
-    if (computerScore === 5){
-                            }
-    if (humanScore === 5){
-                            }
-    if (humanScore >= 5 || computerScore >= 5){
-        humanScore = 0;
-        computerScore = 0;}}
-        
+    if (humanScore === 5){;
+        displayFinal(wonGame);
+        resetScores();}
+    else if (computerScore === 5){
+        resetScores();
+        displayFinal(lostGame);}
+    }
+
+function resetScores(){
+    humanScore = 0;
+    computerScore = 0;
+}
+
+function displayScores()
+{   
+    const containerPlayerScore = document.querySelector("#scorep");
+    const containerComputerScore = document.querySelector("#scorec");
+
+    containerPlayerScore.textContent = `${humanScore} points`;
+    containerComputerScore.textContent = `${computerScore} points`;
+}
+
+function displayPlayed(myHuman)
+{
+    const containerPlayerPlayed = document.querySelector("#p1");
+    const containerComputerPlayed = document.querySelector("#p2");
+
+    containerPlayerPlayed.textContent = `${mainArray[myHuman]}`;
+    containerComputerPlayed.textContent = `${computerChoice}`;
+}
+
+function displayInDivRound(result)
+{
+    const containerResults = document.querySelector("#result");
+    containerResults.textContent = `${result}`;
+}
+
+function displayFinal(result)
+{
+    const containerResultsF = document.querySelector("#result");
+    containerResultsF.textContent = `${result}`;
+}
+
 getHumanChoice();
 
 
