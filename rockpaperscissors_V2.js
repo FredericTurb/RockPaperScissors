2a8781d5f1bc06f1dfefce2c836ab483a3387ad3
FredericTurb <185390564+FredericTurb@users.noreply.github.com>
2024-10-25 02:33:57 +0200

    V2 finished

--- rockpaperscissors_V2.js
+++ rockpaperscissors_V2.js
@@ -6,38 +6,37 @@ const buttonScissors = document.querySelector("#scissors");
 
 let humanScore = 0;
 let computerScore = 0;
-
 let computerChoice = getComputerChoice();
-getHumanChoice();
+
 
 function getComputerChoice(){
     let myRandom = Math.floor(Math.random() * 3);
-    return(mainArray[myRandom]);
-} 
+    return(mainArray[myRandom]);}
+    
 function getHumanChoice(){
-    buttonRock.addEventListener("click", () => playRound(mainArray[0])); 
-    buttonPaper.addEventListener("click", () => playRound(mainArray[1])); 
-    buttonScissors.addEventListener("click", () => playRound(mainArray[2])); 
+    buttonRock.addEventListener("click", () => playGame(0)); 
+    buttonPaper.addEventListener("click", () => playGame(1)); 
+    buttonScissors.addEventListener("click", () => playGame(2));}
+
+function playGame(myHuman){
+    computerChoice = getComputerChoice();
+    if(myHuman === 0 && computerChoice === mainArray[2] || myHuman === 1 && computerChoice === mainArray[0] || myHuman === 2 && computerChoice === mainArray[1]){
+            humanScore = humanScore + 1;
+            checkScores();}
+    else if(myHuman === 0 && computerChoice === mainArray[1] || myHuman === 1 && computerChoice === mainArray[2] || myHuman === 2 && computerChoice === mainArray[0]){
+            computerScore = computerScore + 1;
+            checkScores();}
 }
 
-function playRound(str)
-{
-
-}
-
-
-
-
-
-
-
-
-
-
-// 1) WE GET COMPUTER CHOICE AS AN ARRAY CALLED TEST. TEST[0] = COMPUTER CHOICE. -> DONE
-// 2) CREATE 3 BUTTONS -> ADD EVENT LISTENER                                     -> DONE
-// 3) CALL PLAYROUND WITH THE RESULT OF PRESSED BUTTON AS ARGUMENT               -> DONE
-// 4) 
-
+function checkScores(){
+    if (computerScore === 5){
+                            }
+    if (humanScore === 5){
+                            }
+    if (humanScore >= 5 || computerScore >= 5){
+        humanScore = 0;
+        computerScore = 0;}}
+        
+getHumanChoice();
 
 
